13aaa90038020f7044a2e2c7bf884f4372bb111c
CYunTing <100269151+CYunTing@users.noreply.github.com>
2022-04-23 14:09:17 +0800

    Add files via upload

---
+++ qz5/qz5.js
@@ -0,0 +1,22 @@
+
+/*var myList = document.getElementById('myList');
+myList.addEventListener('click', function(e) {
+  if ( e.target.tagName.toLowerCase() === 'li' ) {
+    console.log(e.target.textContent);
+  }
+}, false);
+
+
+var newList = document.createElement('li');
+var textNode = document.createTextNode("hello cgu!");
+newList.appendChild(textNode);
+myList.appendChild(newList);*/
+
+
+myList.addEventListener('click', function(e) {
+  if ( e.target.tagName.toLowerCase() === 'li' ) {
+    console.log(e.target.textContent);
+  }
+}, false);
+
+$('#myList').append("<li>hello cgu!</li>");
