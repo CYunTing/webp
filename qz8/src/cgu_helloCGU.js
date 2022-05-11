b97757a02341232914fe58793688a1d9923f7e6b
CYunTing <100269151+CYunTing@users.noreply.github.com>
2022-05-11 16:08:01 +0800

    Delete qz8/src directory

--- qz8/src/cgu_helloCGU.js
+++
@@ -1,8 +0,0 @@
-const styleArgument = { fontSize: "100px",color: "red" };
-
-const HelloCGU = () => { 
-    var Hello = <h5 style={styleArgument}>hello CGU!!</h5>
-    return Hello;
-}
-
-export default HelloCGU;
