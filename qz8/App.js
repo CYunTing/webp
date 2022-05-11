a5809bce779e3385bab9edf362a8baeb0af2f3cf
CYunTing <100269151+CYunTing@users.noreply.github.com>
2022-05-11 16:09:09 +0800

    Add files via upload

---
+++ qz8/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import MultiButton from './cgu_multiButton.js';
+import HelloCGU from './cgu_helloCGU';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        { HelloCGU() }
+      </div>
+      <div>
+        { MultiButton(10) } 
+      </div>
+       
+    </div> 
+  ); 
+} 
+    
+
+export default App;
