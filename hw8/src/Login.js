cdbb56b5d89a504f94e9f977682bc27167d97b4a
CYunTing <100269151+CYunTing@users.noreply.github.com>
2022-05-18 17:55:08 +0800

    Update Login.js

--- hw8/src/Login.js
+++ hw8/src/Login.js
@@ -1,12 +1,73 @@
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+
 const Login = () => {
     return (
-        <div>
-            <TextField id='email' label="Email Address"></TextField>
-            <TextField id='password' label="Password"></TextField>
-            <Button variant="contained" color="primary">SI IN</Button>
-        </div>
+        <Grid item xs={12} sm={8} md={5}  elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box component="form" noValidate  sx={{ mt: 1 }}>
+            <h3 margin="center">Sign in CSIE@CGU</h3>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                id="password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+                </Grid>
+                    
+            </Box>
+            </Box>
+        </Grid>
+                
     );
            
     
