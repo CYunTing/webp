2cb8a8ab496d89bbd39b7464de7966c4a51d4061
CYunTing <100269151+CYunTing@users.noreply.github.com>
2022-05-18 17:59:22 +0800

    Add files via upload

---
+++ hw8/src/App.js
@@ -0,0 +1,12 @@
+import Login from './Login.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ hw8/src/button.js
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/Button';
+
+function LoginButton() {
+    return (
+        <Button variant="contained" color="primary">SIGN IN</Button>
+    );
+           
+    
+}
+
+export default LoginButton;
